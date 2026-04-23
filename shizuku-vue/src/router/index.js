b5ced447af3cd0fb8121d08c17f7aebfa265b1ad
Sean <sealll40@gmail.com>
2026-04-23 16:05:29 +0800

    feat: 成功設定路由到Layout

--- shizuku-vue/src/router/index.js
+++ shizuku-vue/src/router/index.js
@@ -1,21 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Basic from '@/layout/Basic.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      layout: Basic,
+      children: [
+        {
+          path: '', // 當路徑為 '/' 時，載入首頁
+          name: 'home',
+          component: () => import('../views/HomeView.vue'), // 假設您有這個檔案
+        },
+        {
+          path: 'about', // 當路徑為 '/about' 時，載入 About 頁面並顯示在 Basic 的 <RouterView /> 內
+          name: 'about',
+          component: () => import('../views/AboutView.vue'),
+        },
+      ],
     },
   ],
 })
--- shizuku-vue/src/views/HomeView.vue
+++ shizuku-vue/src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import Nav from '../components/AppNav.vue'
 </script>
 
 <template>
+  <Nav/>
   <main>
     <TheWelcome />
   </main>
